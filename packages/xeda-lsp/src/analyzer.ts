a74e4bd9b98c8234a7cee8640d45dc8411cfda69
LXYan2333 <z00823823@126.com>
2023-11-20 17:01:35 +0800

    用try catch包住电荷检查

--- packages/xeda-lsp/src/analyzer.ts
+++ packages/xeda-lsp/src/analyzer.ts
@@ -368,7 +368,7 @@ ${fuse_search_result.length === 0 ? '' : `Did you mean ${fuse_search_result[0].i
             }
 
             // 单体电荷之和是否与超分子相等？
-            {
+            try {
                 let all_monomer_charge = this.input_file_info.mcharge.reduce((a, b) => a + b);
                 let supermolecule_charge = this.input_file_info.charge;
                 if (!isNaN(all_monomer_charge) && !isNaN(supermolecule_charge) && all_monomer_charge !== supermolecule_charge) {
@@ -380,6 +380,8 @@ ${fuse_search_result.length === 0 ? '' : `Did you mean ${fuse_search_result[0].i
                         message: `The sum of the charges of all the monomers is ${all_monomer_charge}, but the charge of the supermolecule is ${supermolecule_charge}.`,
                     })
                 }
+            } catch (e) {
+                console.log(e);
             }
 
             let spin_count: { α_spin_electron_count: number, β_spin_electron_count: number }[] = [];
